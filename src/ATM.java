8c5a640190a694849fcf3058536bd012b8983f78
mit-day <timothy.tzx@gmail.com>
2023-02-16 14:14:45 +0800

    Fixed nextline issue for new user sign up

--- src/ATM.java
+++ src/ATM.java
@@ -65,17 +65,13 @@ public class ATM {
                 System.out.print("Enter First name: ");
                 String fname = sc.nextLine();
                 newUser.setFirstName(fname);
-                sc.nextLine();  // Consume newline left-over
 
                 System.out.print("Enter last name: ");
                 String lname = sc.nextLine();
                 newUser.setLastName(lname);
-                sc.nextLine();  // Consume newline left-over
 
                 System.out.print("Enter pin: ");
                 String newPin = sc.nextLine();
-
-                sc.nextLine();  // Consume newline left-over
 
                 // Creates a new user account based on user input
                 User createNewUser = theBank.addUser(fname,lname,newPin);
