e133519fe6d9dc1dccc67062cb8b91e4585ef9b4
kaya0910 <32829552+kaya0910@users.noreply.github.com>
2023-02-23 21:11:45 +0800

    added sql and csv files

---
+++ db/account.sql
@@ -0,0 +1,26 @@
+/*
+-- Query: select * from account
+LIMIT 0, 1000
+
+-- Date: 2023-02-17 21:45
+*/
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (0,0,0,'Savings',223.99);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (1,0,0,'Checkings',9260.49);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (2,0,0,'for my momma',904.07);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (3,1,1,'Alcohol',6689.26);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (4,1,1,'Spending',3677.8);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (5,2,2,'gf expenditure',7223.68);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (6,3,0,'Savings',4574.95);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (7,4,1,'Checkings',3199.57);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (8,5,2,'for my momma',6193.92);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (9,5,2,'Alcohol',9931.34);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (10,5,2,'Spending',5939.81);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (11,6,0,'gf expenditure',2495.76);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (12,6,0,'Savings',8369.33);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (13,7,1,'Checkings',1699.24);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (14,7,1,'for my momma',1283.06);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (15,7,1,'Alcohol',2177.81);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (16,8,2,'Spending',9198.54);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (17,9,0,'gf expenditure',8657.48);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (18,9,0,'Savings',350.64);
+INSERT INTO `` (`idAccount`,`Customer_idCustomer`,`Bank_idBank`,`name`,`balance`) VALUES (19,9,0,'Checkings',2354.97);
