7b4a0c8f149e8adcd144f5acef2702c4bdb6e4e0
alex <alex.lim.xiang.wei@gmail.com>
2023-03-19 16:13:53 +0800

    DONE: fixed account creation

--- src/main/java/com/CLI/ATM/ATM2/service/AccountService.java
+++ src/main/java/com/CLI/ATM/ATM2/service/AccountService.java
@@ -166,6 +166,7 @@ public class AccountService {
             stmt.setDouble(5,bal);
             stmt.executeUpdate();
 
+
             System.out.printf("New account %s created.", name);
         } catch (SQLException e) {
             e.printStackTrace();
