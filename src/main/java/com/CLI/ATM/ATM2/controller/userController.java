196407a71d38e25a09028385838592972513c314
kaya0910 <32829552+kaya0910@users.noreply.github.com>
2023-03-22 21:26:11 +0800

    update html to accept user input

--- src/main/java/com/CLI/ATM/ATM2/controller/userController.java
+++ src/main/java/com/CLI/ATM/ATM2/controller/userController.java
@@ -1,9 +1,8 @@
 package com.CLI.ATM.ATM2.controller;
 import com.CLI.ATM.ATM2.CLI.AccountCLI;
-import com.CLI.ATM.ATM2.model.Account;
-import com.CLI.ATM.ATM2.model.Transaction;
-import com.CLI.ATM.ATM2.model.User;
-import com.CLI.ATM.ATM2.model.UserInput;
+import com.CLI.ATM.ATM2.CLI.MainCLI;
+import com.CLI.ATM.ATM2.model.*;
+import com.CLI.ATM.ATM2.Constants;
 import com.CLI.ATM.ATM2.service.BankService;
 import com.CLI.ATM.ATM2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.sound.midi.SysexMessage;
+
+import static com.CLI.ATM.ATM2.Constants.bankList;
+import static com.CLI.ATM.ATM2.Constants.sc;
 
 @Controller
 public class userController {
@@ -24,22 +29,30 @@ public class userController {
     //signIn page
     @GetMapping("/")
     public String getSignInPage(Model model) {
-        model.addAttribute("userInput", new UserInput());
         return "signInPage";
     }
 
-    @PostMapping("/handleSubmit")
-    public String submitUser(UserInput userInput, Account account, AccountCLI accountCLI) {
+    @PostMapping("/signInSubmit")
+    public String getSignInDetails(@RequestParam("userid") int userid,
+                                   @RequestParam("pin") String pin,
+                                   @RequestParam("bank") int bank){
+        System.out.println(userid);
+        System.out.println(pin);
+        System.out.println(bank);
+
+        
+
+        return "menuPage";
+    }
+
+    @PostMapping("/signUpSubmit")
+    public String submitUser(UserInput userInput, UserService user, User userModel) {
         System.out.println(userInput.getFirstName());
         System.out.println(userInput.getLastName());
         System.out.println(userInput.getPin());
         System.out.println(userInput.getBankId());
 
-        System.out.println(account.getName());
-        System.out.println(account.getAccountID());
-        System.out.println(account.getBalance());
-
-        return "menuPage";
+        return "signInPage";
     }
 
     @GetMapping("/signup")
