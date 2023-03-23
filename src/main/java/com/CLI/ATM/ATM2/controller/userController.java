d70eaf73e1c0ded92f1e754926764eca8c6424d6
kaya0910 <32829552+kaya0910@users.noreply.github.com>
2023-03-23 15:07:22 +0800

    home page added and merged with current sqlService

--- src/main/java/com/CLI/ATM/ATM2/controller/userController.java
+++ src/main/java/com/CLI/ATM/ATM2/controller/userController.java
@@ -1,12 +1,8 @@
 package com.CLI.ATM.ATM2.controller;
 
 import com.CLI.ATM.ATM2.Util;
-import com.CLI.ATM.ATM2.model.Bank;
-import com.CLI.ATM.ATM2.model.User;
-import com.CLI.ATM.ATM2.service.AccountService;
-import com.CLI.ATM.ATM2.service.BankService;
-import com.CLI.ATM.ATM2.service.UserService;
-import com.CLI.ATM.ATM2.service.SQLService;
+import com.CLI.ATM.ATM2.model.*;
+import com.CLI.ATM.ATM2.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static com.CLI.ATM.ATM2.Constants.bankList;
+import static com.CLI.ATM.ATM2.Constants.*;
 
 @Controller
 public class userController {
@@ -33,12 +29,8 @@ public class userController {
 
     //signIn page
     @GetMapping("/")
-    public String getSignInPage(Model model) {
-        return "signInPage";
-    }
-    @GetMapping("/signupPage")
-    public String getSignUpPage() {
-        return "signUpPage";
+    public String home(Model model) {
+        return "home";
     }
 
     @RequestMapping(value = "/signInPage", method = RequestMethod.GET)
@@ -51,6 +43,18 @@ public class userController {
         }
         model.addAttribute("bankListing", bankListing);
         return "signInPage.html";
+    }
+
+    @RequestMapping(value = "/signUpPage", method = RequestMethod.GET)
+    public String populateList2(Model model) {
+        List<Bank> bankListing = sqlService.fetchBanks();
+        for (int i = 0; i < bankListing.size(); i++){
+            if (!bankListing.get(i).isLocal()){
+                bankListing.remove(i);
+            }
+        }
+        model.addAttribute("bankListing", bankListing);
+        return "signUpPage.html";
     }
 
     @PostMapping("/signInPage")
@@ -84,6 +88,7 @@ public class userController {
                              @RequestParam("bankDropdown") int bankid,
                              Model model) {
 
+        populateList2(model);
         System.out.println(firstName);
         System.out.println(lastName);
         System.out.println(pin);
@@ -95,34 +100,23 @@ public class userController {
         String newPin = Util.hash(pin);
         Bank currentBank = bankList.get(bankid);
 
-//        // Creates a new user account based on user input
-//        User newUser2 = bankService.addUserToBank(currentBank, firstName, lastName, newPin,
-//                DEFAULT_LOCAL_TRANSFER_LIMIT, DEFAULT_OVERSEAS_TRANSFER_LIMIT);
-//        Account newAccount2 = accountService.createAccount("CHECKING", newUser2, 0.0);
-//        userService.addAccountToUser(newUser2, newAccount2);
-//        bankService.addAccountToBank(currentBank, newAccount2);
-//
-//        System.out.println("Account successfully created.");
-//
-//        // Add new user to SQL
-//        userService.addNewUser(newUser2.getCustomerID(),firstName,lastName,newPin,currentBank);
-//        accountService.SQL_addAccount(newAccount2.getAccountID(),newUser2.getCustomerID(),currentBank.getBankID(),"Savings",0.00);
-//
-//        System.out.println(newAccount2.getAccountID());
-//        System.out.println(currentBank.getBankID());
+        // Creates a new user account based on user input
+        User newUser2 = bankService.addUserToBank(currentBank, firstName, lastName, newPin,
+                DEFAULT_LOCAL_TRANSFER_LIMIT, DEFAULT_OVERSEAS_TRANSFER_LIMIT);
+        Account newAccount2 = accountService.createAccount("CHECKING", newUser2, 0.0);
+        userService.addAccountToUser(newUser2, newAccount2);
+        bankService.addAccountToBank(currentBank, newAccount2);
+
+        System.out.println("Account successfully created.");
+
+        // Add new user to SQL
+        sqlService.addNewUser(newUser2.getCustomerID(),firstName,lastName,newPin,currentBank,
+                DEFAULT_LOCAL_TRANSFER_LIMIT, DEFAULT_OVERSEAS_TRANSFER_LIMIT);
+        sqlService.addAccount(newAccount2.getAccountID(),newUser2.getCustomerID(),currentBank.getBankID(),"Savings",0.00);
+
+        System.out.println(newAccount2.getAccountID());
+        System.out.println(currentBank.getBankID());
 
         return "signInPage";
-    }
-
-    @RequestMapping(value = "/signUpPage", method = RequestMethod.GET)
-    public String populateList2(Model model) {
-        List<Bank> bankListing = sqlService.fetchBanks();
-        for (int i = 0; i < bankListing.size(); i++){
-            if (!bankListing.get(i).isLocal()){
-                bankListing.remove(i);
-            }
-        }
-        model.addAttribute("bankListing", bankListing);
-        return "signUpPage.html";
     }
 }
