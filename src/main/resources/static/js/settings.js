9f7c517f8944c1a6ff03956b71c7cf025b6e7ef1
kaya0910 <32829552+kaya0910@users.noreply.github.com>
2023-03-25 16:43:11 +0800

    password div update

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -4,7 +4,20 @@
     showDivButton.addEventListener("click", () => {
         if (createAccDiv.style.display === "none") {
             createAccDiv.style.display = "block";
+            passwordDiv.style.display = "none"
         } else {
             createAccDiv.style.display = "none";
+        }
+    });
+
+    const showDiv2Button = document.getElementById("password-change");
+    const passwordDiv = document.getElementById("password-div");
+
+    showDiv2Button.addEventListener("click", () => {
+        if (passwordDiv.style.display === "none") {
+            passwordDiv.style.display = "block";
+            createAccDiv.style.display = "none";
+        } else {
+            passwordDiv.style.display = "none";
         }
     });
